3eacfd7a8b03320dc384786f7f30ba2562c12e56
Gernot Vormayr <gvormayr@gmail.com>
2019-03-10 18:54:15 +0100

    Prevent inlining the libcCall1-6 functions
    Without this function arguments are not in the place asmlibccall6
    expects them (one after another on the stack) in go1.12 due to function
    inlining.

--- fakecgo/libccall.go
+++ fakecgo/libccall.go
@@ -12,31 +12,37 @@ func libcCall0(fn *libcFunc) uintptr {
 }
 
 //go:nosplit
+//go:noinline
 func libcCall1(fn *libcFunc, a1 uintptr) uintptr {
 	return asmlibccall6(uintptr(unsafe.Pointer(fn)), 1, uintptr(unsafe.Pointer(&a1)))
 }
 
 //go:nosplit
+//go:noinline
 func libcCall2(fn *libcFunc, a1 uintptr, a2 uintptr) uintptr {
 	return asmlibccall6(uintptr(unsafe.Pointer(fn)), 2, uintptr(unsafe.Pointer(&a1)))
 }
 
 //go:nosplit
+//go:noinline
 func libcCall3(fn *libcFunc, a1 uintptr, a2 uintptr, a3 uintptr) uintptr {
 	return asmlibccall6(uintptr(unsafe.Pointer(fn)), 3, uintptr(unsafe.Pointer(&a1)))
 }
 
 //go:nosplit
+//go:noinline
 func libcCall4(fn *libcFunc, a1 uintptr, a2 uintptr, a3 uintptr, a4 uintptr) uintptr {
 	return asmlibccall6(uintptr(unsafe.Pointer(fn)), 4, uintptr(unsafe.Pointer(&a1)))
 }
 
 //go:nosplit
+//go:noinline
 func libcCall5(fn *libcFunc, a1 uintptr, a2 uintptr, a3 uintptr, a4 uintptr, a5 uintptr) uintptr {
 	return asmlibccall6(uintptr(unsafe.Pointer(fn)), 5, uintptr(unsafe.Pointer(&a1)))
 }
 
 //go:nosplit
+//go:noinline
 func libcCall6(fn *libcFunc, a1 uintptr, a2 uintptr, a3 uintptr, a4 uintptr, a5 uintptr, a6 uintptr) uintptr {
 	return asmlibccall6(uintptr(unsafe.Pointer(fn)), 6, uintptr(unsafe.Pointer(&a1)))
 }
